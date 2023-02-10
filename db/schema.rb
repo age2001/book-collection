8fdf37f48896f05920481cea4cbf4f9b5d3a711a
Alan Enriquez <alan.enriquez2001@gmail.com>
2023-02-10 17:21:55 -0600

    Changing initial schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_023619) do
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.decimal "price"
-    t.date "published"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
